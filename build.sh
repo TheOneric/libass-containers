2432bf9363e39b9e7c4344eff77ef6109c09e35e
Oneric <oneric@oneric.stub>
2022-11-12 03:46:44 +0100

    [WIP] build: bump FriBidi to 0.19.7
    Needed for the threading patches.

--- build.sh
+++ build.sh
@@ -38,8 +38,8 @@ cd /tmp/build
 
 ## GNU FriBidi
 ### 0.19.1 is the first "fribidi2" release, 0.19.0 was an indev version used from 2004-2008
-urlfmt='https://github.com/fribidi/fribidi/releases/download/FRIBIDI_0_19_1/fribidi-%s.tar.gz'
-tarball_build fribidi "$urlfmt" "0.19.1"
+urlfmt='https://github.com/fribidi/fribidi/releases/download/0.19.7/fribidi-%s.tar.bz2'
+tarball_build fribidi "$urlfmt" "0.19.7"
 
 ## FreeType2 (without HarfBuzz)
 ### Internal version 9.17.3 == external version 2.3.6
